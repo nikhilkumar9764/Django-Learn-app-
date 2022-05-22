75d2284fe84644fadd3670cd5583e0d2d6bef1f6
NIKHIL KUMAR <nikhil.kumar9764@gmail.com>
2022-05-22 14:31:47 +0530

    App first commit

--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
 # Create your views here.
 from .models import Youtuber
 
@@ -12,7 +12,20 @@ def youtubers(request):
     return render(request, 'youtubers/youtubers.html', data)
 
 def youtubers_detail(request , id):
-    return render(request, 'youtubers/youtuber_detail.html')
+    tuber = get_object_or_404(Youtuber, pk=id)
+    data = {
+        'tuber': tuber
+    }
+    return render(request, 'youtubers/youtuber_detail.html', data)
 
 def search(request):
-    pass
+    tubers = Youtuber.objects.order_by('-created_date')
+    if 'keyword' in request.GET:
+        search_token = request.GET['keyword']
+        if search_token:
+           tubers = Youtuber.objects.filter(description__icontains=search_token)
+
+    data = {
+        'tubers' : tubers
+    }
+    return render(request, 'youtubers/search.html', data)
